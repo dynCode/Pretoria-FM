c65e20c7f7acbb9cc3335f7b42b6fe39f5411be8
Llewellyn Dawson <llewellyn@myitmanager.co.za>
2018-03-16 15:00:56 +0200

    Try Now

--- js/app.js
+++ js/app.js
@@ -7,23 +7,27 @@
     module.controller('AppController', function ($scope, $http, $window, $rootScope, $sce) {
         $scope.streamURL = $sce.trustAsResourceUrl('http://myitmanager.co.za/pretoriafm/player.html');
         $scope.mapURL = $sce.trustAsResourceUrl('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3593.7539543474313!2d28.29189631560485!3d-25.745649051741147!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1ebfdf1f40e32beb%3A0x2b680ca4d452012e!2sRadio+Pretoria!5e0!3m2!1sen!2sus!4v1456435667117');
+        
+        $scope.playAudio = function() {
+            // Play the audio file at url
+            var my_media = new Media("http://capeant.antfarm.co.za:1935/pretoriafm/pretoriafm.stream/playlist.m3u8",
+                // success callback
+                function () {
+                    console.log("playAudio():Audio Success");
+                },
+                // error callback
+                function (err) {
+                    console.log("playAudio():Audio Error: " + err);
+                }
+            );
+            // Play audio
+            my_media.play();
+        };
     });
 })();
 
 // normal JS //
 document.addEventListener('deviceready', function () {
-    // Android customization
-    cordova.plugins.backgroundMode.setDefaults({ text:'Play PTA FM.'});
-    // Enable background mode
-    cordova.plugins.backgroundMode.enable();
-
-    // Called when background mode has been activated
-    cordova.plugins.backgroundMode.onactivate = function () {
-        setTimeout(function () {
-            // Modify the currently displayed notification
-            cordova.plugins.backgroundMode.configure({
-                text:'Playing PTA FM.'
-            });
-        }, 5000);
-    }
+    
 }, false);
+
